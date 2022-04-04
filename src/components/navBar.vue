7c772277fb5b6b8c585b6afaeaae130e0b8d8621
kevin <kevinli8831@gmail.com>
2022-04-04 10:28:10 +0800

    fix z-index

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex justify-center sm:justify-end px-5 sm:px-10 py-5 w-full fixed bg-[#071425] mx-auto"
+    class="flex justify-center sm:justify-end px-5 sm:px-10 py-5 w-full fixed bg-[#071425] mx-auto z-10"
   >
     <div class="sm:space-x-10 space-x-2">
       <a
